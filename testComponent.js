f18439fc8c73e9650646fe1ded86d539881006ae
Mario Fink <mario.fink@goodyear.com>
2018-09-14 13:07:34 +0200

    basic custom element

---
+++ testComponent.js
@@ -0,0 +1,26 @@
+class TestComponent extends HTMLElement {
+  constructor() {
+    // If you define a constructor, always call super() first!
+    // This is specific to CE and required by the spec.
+    super();
+
+    this.addEventListener("click", e => {
+      console.log("I have been clicked", this);
+      this.setAttribute("disabled", true);
+    });
+  }
+  static get observedAttributes() {
+    return ["disabled"];
+  }
+  connectedCallback() {
+    console.log("Gonna get myself connected");
+  }
+  disconnectedCallback() {
+    console.log("You have been disconnected");
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    console.log("attribute changed", attrName);
+  }
+}
+
+window.customElements.define("test-component", TestComponent);
