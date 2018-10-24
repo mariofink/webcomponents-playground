40e7123ebe7f2dbf301b123f9fcdf66029095641
Mario Fink <mario.fink@goodyear.com>
2018-10-24 13:27:06 +0200

    react to change of notification type

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,6 +1,9 @@
 export default class Notification extends HTMLElement {
   constructor() {
     super();
+
+    this.notificationTypeDOMElement = document.createElement("span");
+    this.notificationTypeDOMElement.classList.add("my-notification__type");
   }
 
   static get observedAttributes() {
@@ -8,6 +11,14 @@ export default class Notification extends HTMLElement {
   }
 
   attributeChangedCallback(attrName, oldVal, newVal) {
-    console.log("attribute changed", attrName);
+    if (attrName === "type") {
+      this.typeChangedHandler(newVal);
+    }
+  }
+
+  typeChangedHandler(value) {
+    console.log("type has been changed");
+    this.notificationTypeDOMElement.innerText = value;
+    this.prepend(this.notificationTypeDOMElement);
   }
 }
