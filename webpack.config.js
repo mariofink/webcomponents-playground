c9aa1ab9969b83ab4ff60b0bb005eaf0e039d1bc
Mario Fink <mario.fink@goodyear.com>
2018-11-02 09:08:17 +0100

    extract CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const devMode = process.env.NODE_ENV !== "production";
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,6 +21,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Web Components Playground",
       template: "src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
     })
   ],
   module: {
@@ -38,7 +46,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          "style-loader", // creates style nodes from JS strings
+          MiniCssExtractPlugin.loader,
           "css-loader", // translates CSS into CommonJS
           "sass-loader" // compiles Sass to CSS, using Node Sass by default
         ]
