81b68533a5359b8adbb3100cabd97c38516e0aab
Mario Fink <mario.fink@goodyear.com>
2020-04-24 11:03:10 +0200

    add first LitElement component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "@webcomponents/custom-elements";
 
 import TestComponent from "./components/testComponent";
 import Notification from "./components/notification/notification";
+import LitComponent from "./components/litComponent";
 
 window.customElements.define("test-component", TestComponent);
 window.customElements.define("my-notification", Notification);
+window.customElements.define('lit-component', LitComponent);
