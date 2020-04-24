1261d65534de9ea76fec6c0988dcab0ae9884ae1
Mario Fink <mario.fink@goodyear.com>
2020-04-24 13:07:16 +0200

    use classMap for making class dynamic depending on type

--- src/components/notification/litNotification.js
+++ src/components/notification/litNotification.js
@@ -1,4 +1,6 @@
 import { LitElement, html, css } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+
 const icons = {
   info: require("./assets/info.svg"),
   warning: require("./assets/warning.svg"),
@@ -24,12 +26,48 @@ export default class LitNotification extends LitElement {
         vertical-align: middle;
         fill: currentColor;
       }
+
+      .info {
+        background: #118ab2;
+        color: white;
+      }
+
+      .warning {
+        background: #ffd166;
+        color: #333;
+      }
+
+      .error {
+        background: #ef476f;
+        color: white;
+      }
+
+      .success {
+        background: #06d6a0;
+        color: #333;
+      }
     `;
   }
 
+  static get properties() {
+    return { 
+      type: { type: String },
+      dismissable: { type: Boolean }
+    };
+  }
+
+  constructor() {
+    super();
+    // initialise properties
+    this.type = "info";
+    this.dismissable = false;
+  }
+
   render(){
+    const classes = {};
+    classes[this.type] = true;
     return html`
-    <div class="container">
+    <div class="container ${classMap(classes)}">
       <img src="data:image/svg+xml;utf8,${icons.info}" alt="icon">
       <slot></slot>
     </div>
