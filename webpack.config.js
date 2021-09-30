b98f175b3fd8d47a8efc89cdca9aec33dd9e0c49
Mario Fink <mario.fink@goodyear.com>
2021-09-30 14:02:54 +0200

    update packages, except lit-element

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const devMode = process.env.NODE_ENV !== "production";
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -9,25 +9,25 @@ module.exports = {
   mode: "development",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
     host: "0.0.0.0",
     port: 7890,
-    contentBase: "./dist"
+    static: ["dist"],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Web Components Playground",
-      template: "src/index.html"
+      template: "src/index.html",
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: devMode ? "[name].css" : "[name].[hash].css",
-      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
-    })
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+    }),
   ],
   module: {
     rules: [
@@ -39,30 +39,30 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
-        test:/\.css$/,
+        test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader", // translates CSS into CommonJS
-        ]
+        ],
       },
       {
         test: /\.scss$/,
         use: [
           "raw-loader", // use raw-loader as LitElement will put it in the static styles property
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
+        ],
       },
       {
         test: /\.svg$/,
         use: {
-          loader: "svg-inline-loader"
-        }
-      }
-    ]
-  }
+          loader: "svg-inline-loader",
+        },
+      },
+    ],
+  },
 };
